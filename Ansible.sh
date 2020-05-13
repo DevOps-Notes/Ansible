91991dc61b99c3ec7a1a2fdfc0e1a9ffbfa522ab
Warren Luo <luo.wei@oe.21vianet.com>
2020-05-13 16:04:44 +0800

    Ansible.sh updated

--- Ansible.sh
+++ Ansible.sh
@@ -9,5 +9,12 @@ yum -y install net-tools
 systemctl stop firewalld && systemctl disable firewalld
 # Disable selinux
 setenforce 0 && vim /etc/selinux/config # SELINUX=disabled
-# install dependencies
+# Install dependencies
 yum -y install gcc gcc-c++ ncurses ncurses-devel zlib zlib-devel openssl openssl-devel
+# Install Python 3.6
+cd /usr/local/src/ && wget 'https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz' && tar -zxf Python-3.6.4.tgz
+cd Python-3.6.4 && ./configure --prefix=/usr/local/python/ && make && make install
+ln -s /usr/local/python/bin/python3 /usr/local/bin/ && python3 --version
+/usr/local/python/bin/pip3 install --upgrade pip
+# Install ansible
+/usr/local/python/bin/pip3 install ansible && ln -s /usr/local/python/bin/ansible /usr/local/bin/ && ansible --version
