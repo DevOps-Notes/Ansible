57cc98644c54caabfe707a5e7963bf56eb6e8f1d
Warren Luo <luo.wei@oe.21vianet.com>
2020-05-15 10:12:01 +0800

    05.Ansible_FastKeyDistribution.sh updated

--- 05.Ansible_FastKeyDistribution.sh
+++ 05.Ansible_FastKeyDistribution.sh
@@ -6,7 +6,7 @@ ServerName ansible_ssh_port=22 ansible_ssh_host=ServerIP ansible_ssh_pass='*****
 
 vim /etc/ssh/ssh_config
 pubkey=`cat /root/.ssh/id_rsa.pub`
-ansible all -m shell -a "cd /root/; umask 077; test -d .ssh || mkdir .ssh ; echo ${pubkey} >> .ssh/authorized_keys"
+ansible all -m shell -a "cd /root/; umask 077; test -d .ssh || mkdir .ssh; echo ${pubkey} >> .ssh/authorized_keys;"
 
 # ->
 
